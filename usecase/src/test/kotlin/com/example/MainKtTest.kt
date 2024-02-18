2ee6b8382eb9b5c99125bfb6bacbfaab4322fdc9
jnuank <ikuta1919@gmail.com>
2024-02-18 20:43:04 +0900

    usecaseのテストを動かせるように

---
+++ usecase/src/test/kotlin/com/example/MainKtTest.kt
@@ -0,0 +1,18 @@
+package com.example
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class MainKtTest {
+
+    @Test
+    fun main() {
+    }
+
+    @Test
+    fun テスト() {
+        assertTrue(true)
+    }
+
+}
